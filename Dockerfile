fbcd4b9299b69150f4d3b05874d3ad5c4ef9a2b9
Alex Gogl <gogl.alex@gmail.com>
2019-01-19 19:12:54 +0000

    working local dev environment

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.7-alpine3.8
 
-COPY .app/requirements.txt /app
+COPY ./app/requirements.txt /app
 WORKDIR /app/
 
-RUN pip install -r requirements.txt
+#--virtual groups build packages with the name build-dependencies to remove later
+RUN apk add --update --no-cache --virtual \
+    build-dependencies \
+    python3-dev \
+    build-base \
+    libffi-dev \
+    openssl-dev \
+    libxml2-dev \
+    libxslt-dev \
+    && pip install -r requirements.txt
+# && apk del build-dependencies
 
-COPY ./app /app
+#COPY ./app /app
---
+++ app/quotes_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.xpath('span/small/text()').extract_first(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+process.crawl(QuotesSpider)
+process.start() # the script will block here until the crawling is finished
